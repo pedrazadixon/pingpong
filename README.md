2293624942a08df9a39478e5ce0ed5fc091b6cfd
Dixon Pedraza <pedrazadixon@gmail.com>
2023-06-23 12:32:36 -0500

    pingpong

---
+++ README.md
@@ -0,0 +1,9 @@
+# PingPong - An UDP port test tool
+
+1. Up a server in a especific port
+
+`python pingpong.py listen --bind=0.0.0.0 --port=15000 --proto=udp`
+
+2. ping this server (change the ip for your server ip)
+
+`python pingpong.py ping --server=192.168.2.100 --port=15000 --proto=udp`
---
+++ pingpong.py
@@ -0,0 +1,99 @@
+import socket
+import argparse
+import signal
+import sys
+import time
+
+
+def run_udp_server(bind_ip, port):
+    # Crea un socket UDP
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Asocia el socket a la dirección IP y puerto especificados
+    udp_socket.bind((bind_ip, port))
+
+    print(f"Servidor UDP escuchando en {bind_ip}:{port}")
+
+    # Función de manejo de la señal de interrupción (Ctrl+C)
+    def signal_handler(sig, frame):
+        print("\nstopping...")
+        udp_socket.close()
+        sys.exit(0)
+
+    # Registra el manejador de señal
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Establece un tiempo de espera en el socket del servidor
+    udp_socket.settimeout(1.0)  # Tiempo de espera de 1 segundo
+
+    while True:
+        try:
+            # Espera a recibir datos del cliente
+            data, address = udp_socket.recvfrom(1024)
+            print(f"{data.decode()} from {address[0]}:{address[1]}")
+
+            # Responde al cliente
+            print(f"Pong to {address[0]}:{address[1]}")
+            response = "Pong"
+            udp_socket.sendto(response.encode(), address)
+
+        except:
+            pass
+
+
+def run_udp_client(server_ip, port):
+    # Crea un socket UDP para el cliente
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Establece un tiempo de espera en el socket del cliente
+    client_socket.settimeout(1.0)  # Tiempo de espera de 1 segundo
+
+    # Función de manejo de la señal de interrupción (Ctrl+C)
+    def signal_handler(sig, frame):
+        print("\nstopping...")
+        client_socket.close()
+        sys.exit(0)
+
+    # Registra el manejador de señal
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Establece un tiempo de espera en el socket
+    client_socket.settimeout(1.0)  # Tiempo de espera de 1 segundo
+
+    while True:
+        try:
+            # Datos a enviar al servidor
+            print(f"Ping to {server_ip}:{port}")
+            message = "Ping"
+            client_socket.sendto(message.encode(), (server_ip, port))
+
+            # Espera a recibir la respuesta del servidor
+            response, server_address = client_socket.recvfrom(1024)
+            print(
+                f"{response.decode()} from {server_address[0]}:{server_address[1]}")
+
+        except:
+            print("Connection timed out")
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cliente/Servidor UDP')
+    parser.add_argument('action', choices=[
+                        'listen', 'ping'], help='Acción a realizar')
+    parser.add_argument('--bind', default='0.0.0.0',
+                        help='Dirección IP para escuchar (solo para acción "listen")')
+    parser.add_argument('--port', type=int, default=15000, help='Puerto UDP')
+    parser.add_argument('--proto', default='udp', help='Protocolo')
+    parser.add_argument(
+        '--server', help='Dirección IP del servidor (solo para acción "ping")')
+
+    args = parser.parse_args()
+
+    if args.action == 'listen' and args.proto == 'udp':
+        run_udp_server(args.bind, args.port)
+    elif args.action == 'ping' and args.proto == 'udp' and args.server:
+        run_udp_client(args.server, args.port)
+    else:
+        print("Invalid command")
